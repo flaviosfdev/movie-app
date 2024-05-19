39db0b1cc9aa33631632870ac6f4dd31a4b179e1
Flavio Ferreira <flaviosf.dev@gmail.com>
2024-05-19 11:46:05 -0300

    feat: add util functions

--- app/src/main/java/br/com/movieapp/framework/util/ResultData.kt
+++ app/src/main/java/br/com/movieapp/framework/util/ResultData.kt
@@ -4,4 +4,4 @@ sealed class ResultData<out T> {
     object Loading : ResultData<Nothing>()
     data class Success<out T>(val data: T) : ResultData<T>()
     data class Failure(val e: Exception?) : ResultData<Nothing>()
-} 
+}
